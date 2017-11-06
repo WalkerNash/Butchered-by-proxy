e5042067c7a3f8be6d6a2b6e75a41737589421bc
Raven <ravenshooper@gmail.com>
2017-11-06 15:36:06 -0500

    megabot javascript

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,28 @@
 console.log('Hello world');
+
+//Megabot JS
+var card =  document.querySelector('.card');
+
+var c1 =  document.querySelector('.c1');
+var c2 =  document.querySelector('.c2');
+var c3 =  document.querySelector('.c3');
+
+var meetTeam =  document.querySelector('.meetTeam');
+var aboutMega = document.querySelector('.aboutMega');
+
+var count = 0;
+
+function expandCard1() {
+    c1.classList.toggle('hidden');
+    meetTeam.classList.toggle('hidden');
+}
+
+function expandCard2() {
+    c2.classList.toggle('hidden');
+    aboutMega.classList.toggle('hidden');
+}
+
+function expandCard3() {
+    c3.classList.toggle('hidden');
+    //aboutMega.classList.toggle('hidden');
+}
