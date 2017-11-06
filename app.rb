f2b79dc4371dda541554801a7b0daa56a288ce29
Hillary Charen <hillary.charen@gmail.com>
2017-11-06 14:20:51 -0500

    "api"

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sendgrid-ruby'
+
+include SendGrid
+
 
 get '/' do
   erb :index
@@ -15,4 +19,18 @@ end
 
 get '/megabots' do
   erb :megabots
+end
+
+post '/' do
+from = Email.new(email: 'test@example.com')
+to = Email.new(email: 'test@example.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
 end
